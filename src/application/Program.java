2a85ee03993549f72c986c51207e7dfb7bcfc731
Arthur Acrani <arthuracrani12@gmail.com>
2024-01-23 22:09:34 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +15,10 @@ public class Program {
 		System.out.println(obj);
 
 		Seller seller = new Seller(25, "arthur", "arthuracrani12@gmail.com", new Date(), 3000.0, obj);
+		
+		//uma forma de fazer uma injeção de dependencia sem conhecer a implementação apenas a interface
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 	}
 
